c60c24a77fbfbeebe80dab2be73d6dfad02f2dd1
caixiaoeri08@163.com <caixiaowei08@163.com>
2017-07-18 17:48:41 +0800

    '20170718'

--- webpage/login/login.js
+++ webpage/login/login.js
@@ -1,37 +1,65 @@
 /**
  * Created by User on 2017/5/24.
  */
-function login(){
+function login() {
     setCookie();
-    window.location.href='../main/ace_main.html';
+    var accountId = $("#accountId").val();
+    var accountPwd = $("#accountPwd").val();
+    if (accountId === null || accountId === undefined || accountId === '') {
+        toastr.error("请输入账号！");
+        return;
+    }
+    if (accountPwd === null || accountPwd === undefined || accountPwd === '') {
+        toastr.error("请输入密码！");
+        return;
+    }
+    $.ajax({
+        type: "post",
+        url: GLOBAL_CONSTANT_URL + "adminController.do?doLogin",
+        type: 'POST',
+        data: {accountId: accountId, accountPwd: accountPwd},
+        success: function (res) {
+            if (res.success === "success") {
+                window.location.href = '../main/ace_main.html';
+            } else if (res.success === "fail") {
+                toastr.error(res.msg);
+            }
+        },
+        error: function () {
+            toastr.warning("系统异常，请联系运维!");
+        },
+        complete: function (XMLHttpRequest, textStatus) {
+            if (status === 'timeout') {//超时,status还有success,error等值的情况
+                toastr.warning("登录超时！");
+            }
+        }
+    }, 'json');
 }
 
 //设置cookie
-function setCookie()
-{
+function setCookie() {
     if ($('#on_off')[0].checked) {
         localStorage.setItem("COOKIE_NAME", $("#accountId").val());
         localStorage.setItem("COOKIE_ON_OFF", $("#on_off")[0].checked);
-    }else{
+    } else {
         localStorage.setItem("COOKIE_NAME", "");
         localStorage.setItem("COOKIE_ON_OFF", $("#on_off")[0].checked);
     }
 }
 
-function getCookie()
-{
-        $("#accountId").val(localStorage.getItem("COOKIE_NAME"));
-        $("#on_off").attr("checked",(localStorage.getItem("COOKIE_ON_OFF"))) ;
-        if(localStorage.getItem("COOKIE_ON_OFF") === "true"){
-            $("#on_off").prop("checked", true);
-        }else{
-            $("#on_off").prop("checked", false);
-        }
+function getCookie() {
+    $("#accountId").val(localStorage.getItem("COOKIE_NAME"));
+    $("#on_off").attr("checked", (localStorage.getItem("COOKIE_ON_OFF")));
+    if (localStorage.getItem("COOKIE_ON_OFF") === "true") {
+        $("#on_off").prop("checked", true);
+    } else {
+        $("#on_off").prop("checked", false);
+    }
 }
 
 $(function () {
     getCookie();
-    toastr.options ={
+    toastr.options = {
         "closeButton": false,
         "debug": false,
         "newestOnTop": false,
--- webpage/main/ace_main.js
+++ webpage/main/ace_main.js
@@ -1,6 +1,46 @@
-function AppViewModel() {
-    this.accountId = "admin";
-    this.role = "管理员";
-}
-ko.applyBindings(new AppViewModel());
+$(function () {
+    var appViewModel = {
+        accountId: ko.observable(),
+        role: ko.observable()
+    }
+    ko.applyBindings(appViewModel);
 
+    $.ajax({
+        type: "post",
+        url: GLOBAL_CONSTANT_URL + "adminController.do?getAdminEntityFromSession",
+        success: function (res) {
+            if (res.success === "success") {
+                appViewModel.accountId(res.content.accountId);
+                if (res.content.type === 1) {
+                    appViewModel.role("管理员");
+                } else if (res.content.type === 2) {
+                    appViewModel.role("录入员");
+                }
+            } else {
+                window.location.href = '../login/login.html';
+            }
+        },
+        error: function () {
+            toastr.error("服务器异常，请联系维护人员！");
+            window.location.href = '../login/login.html';
+        },
+        complete: function (XMLHttpRequest, textStatus) {
+            if (status === 'timeout') {//超时,status还有success,error等值的情况
+                win.toastr.error("获取课程信息超时,请检查网络问题！");
+            }
+        }
+    }, 'json');
+});
+
+function loginOff() {
+    $.ajax({
+        type: "post",
+        url: GLOBAL_CONSTANT_URL + "adminController.do?loginOff",
+        success: function (res) {
+            window.location.href = '../login/login.html';
+        },
+        error: function () {
+            toastr.error("服务器异常，请联系维护人员！");
+        }
+    }, 'json');
+}
